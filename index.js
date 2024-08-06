155ffcc03a503e260fc25358064b5ffd3610c471
codic-yeeshu <yeeshu.exploresmore@gmail.com>
2024-08-06 15:30:52 +0530

    updated required functions

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-function logInfo(message) {
+const fs = require("fs");
 
+function logInfo(message) {
+  fs.appendFileSync(
+    "./logs/app.log",
+    `${new Date().toISOString()} | INFO | ${message}\n\n`
+  );
 }
 
 function logWarning(message) {
-
+  fs.appendFileSync(
+    "./logs/app.log",
+    `${new Date().toISOString()} | WARNING | ${message}\n\n`
+  );
 }
 
 function logError(message) {
-
+  fs.appendFileSync(
+    "./logs/app.log",
+    `${new Date().toISOString()} | ERROR | ${message}\n\n`
+  );
 }
 
 logInfo("This is an info message");
